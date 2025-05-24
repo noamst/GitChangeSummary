ded7b7b63bb489b8dd82004e1cc9ba6af02cb1ff
Noam Stopler <noam.stopler@gmail.com>
2025-05-24 14:07:26 +0300

    another try

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is missing")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
